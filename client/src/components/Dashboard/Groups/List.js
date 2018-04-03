c0e7d99ca742f44c9156576b43d25b6ff92a8370
Quernest <a.zhit4enko@gmail.com>
2018-04-03 09:13:51 +0300

    external list component

--- client/src/components/Dashboard/Groups/List.js
+++ client/src/components/Dashboard/Groups/List.js
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types';
 const List = ({
  items, fetching, onRemove, onEdit 
 }) => (
-  <ul className="dashboard-groups__list">
+  <ul className="list">
     {!fetching &&
       items &&
       items.map((group, index) => (
-        <li className="dashboard-groups__list-item" key={index}>
+        <li className="list__item" key={index}>
           <span>{group.name}</span>
-          <div className="dashboard-groups__list-item-controls">
+          <div className="list__item-controls">
             <button
-              className="dashboard-groups__list-item-edit"
+              className="list__item-edit"
               onClick={() => onEdit(group.id)}
             />
             <button
-              className="dashboard-groups__list-item-remove"
+              className="list__item-remove"
               onClick={() => onRemove(group.id)}
             />
           </div>
