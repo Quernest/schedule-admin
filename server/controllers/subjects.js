1e9dfff7a770a77254113e8205bc4aa5b7fe875d
Quernest <a.zhit4enko@gmail.com>
2018-04-28 20:09:35 +0300

    subjects error handlers

--- server/controllers/subjects.js
+++ server/controllers/subjects.js
@@ -1,40 +1,48 @@
 const subjectsModel = require('../models/subjects');
 
 module.exports.getAll = (req, res) => {
-  subjectsModel.getAll((err, rows) => {
-    if (err) throw err;
-
-    res.send(rows);
+  subjectsModel.getAll((error, rows) => {
+    if (error) {
+      res.status(400).send({ error });
+    } else {
+      res.send(rows);
+    }
   });
 };
 
 module.exports.getById = (req, res) => {
   const { id } = req.params;
 
-  subjectsModel.getById(id, (err, rows) => {
-    if (err) throw err;
-
-    res.send(rows);
+  subjectsModel.getById(id, (error, rows) => {
+    if (error) {
+      res.status(400).send({ error });
+    } else {
+      res.send(rows);
+    }
   });
 };
 
 module.exports.edit = (req, res) => {
   const { body } = req;
 
-  subjectsModel.edit(body, (err, rows) => {
-    if (err) throw err;
-
-    res.send(rows);
+  subjectsModel.edit(body, (error, rows) => {
+    if (error) {
+      res.status(400).send({ error });
+    } else {
+      res.send(rows);
+    }
   });
 };
 
 module.exports.add = (req, res) => {
   const { body } = req;
 
-  subjectsModel.add(body, (err, rows) => {
-    if (err) throw err;
-
-    res.send(rows);
+  subjectsModel.add(body, (error, rows) => {
+    if (error) {
+      res.status(400).send({ error });
+    } else {
+      res.send(rows);
+    }
   });
 };
 
@@ -42,9 +50,11 @@ module.exports.remove = (req, res) => {
   const { body } = req;
   const { id } = body;
 
-  subjectsModel.remove(id, (err, rows) => {
-    if (err) throw err;
-
-    res.send(rows);
+  subjectsModel.remove(id, (error, rows) => {
+    if (error) {
+      res.status(400).send({ error });
+    } else {
+      res.send(rows);
+    }
   });
 };
--- server/models/subjects.js
+++ server/models/subjects.js
@@ -1,8 +1,10 @@
 const database = require('../config/database');
 
 module.exports.getAll = (callback) => {
-  database.pool.query('SELECT * FROM subjects ORDER BY subjects.name ASC', (err, rows) => {
-    if (err) throw err;
+  database.pool.query('SELECT * FROM subjects ORDER BY subjects.name ASC', (error, rows) => {
+    if (error) {
+      return callback(error, {});
+    }
 
     return callback(null, rows);
   });
@@ -10,8 +12,10 @@ module.exports.getAll = (callback) => {
 
 module.exports.getById = (id, callback) => {
   if (id) {
-    database.pool.query(`SELECT * FROM subjects WHERE subjects.id = ${id}`, (err, rows) => {
-      if (err) throw err;
+    database.pool.query(`SELECT * FROM subjects WHERE subjects.id = ${id}`, (error, rows) => {
+      if (error) {
+        return callback(error, {});
+      }
 
       const [subject] = rows;
 
@@ -23,45 +27,43 @@ module.exports.getById = (id, callback) => {
 module.exports.edit = (body, callback) => {
   const { id } = body;
 
-  database.pool.query(
-    `UPDATE subjects SET ? WHERE subjects.id = ${id}`,
-    body,
-    (err, rows) => {
-      if (err) throw err;
+  database.pool.query(`UPDATE subjects SET ? WHERE subjects.id = ${id}`, body, (error, rows) => {
+    if (error) {
+      return callback(error, {});
+    }
 
-      return callback(null, rows);
-    },
-  );
+    return callback(null, rows);
+  });
 };
 
 module.exports.add = (body, callback) => {
   const { name, type } = body;
 
-  database.pool.query(
-    'INSERT INTO subjects SET ?',
-    body,
-    (err, rows) => {
-      if (err) throw err;
+  database.pool.query('INSERT INTO subjects SET ?', body, (error, rows) => {
+    if (error) {
+      return callback(error, {});
+    }
 
-      if (rows) {
-        database.pool.query(
-          `SELECT * FROM subjects s WHERE s.name = "${name}" AND s.type = "${type}"`,
-          (err, subjects) => {
-            if (err) throw err;
+    if (rows) {
+      database.pool.query(
+        `SELECT * FROM subjects s WHERE s.name = "${name}" AND s.type = "${type}"`,
+        (err, subjects) => {
+          if (err) throw err;
 
-            const [subject] = subjects;
+          const [subject] = subjects;
 
-            return callback(null, subject);
-          },
-        );
-      }
-    },
-  );
+          return callback(null, subject);
+        },
+      );
+    }
+  });
 };
 
 module.exports.remove = (id, callback) => {
-  database.pool.query(`DELETE FROM subjects WHERE subjects.id = ${id}`, (err, rows) => {
-    if (err) throw err;
+  database.pool.query(`DELETE FROM subjects WHERE subjects.id = ${id}`, (error, rows) => {
+    if (error) {
+      return callback(error, {});
+    }
 
     return callback(null, rows);
   });
